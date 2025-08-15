fa6219b44be19c5b63dd3357bf70d1d79ca517a7
Thân <minhthan007@gmail.com>
2025-08-15 23:02:02 +0700

    fix: build docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: "standalone",
 };
-
-export default nextConfig;
